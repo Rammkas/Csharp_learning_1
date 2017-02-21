c42c84820d06fe617ac9387bc3e1d4077ca9d3be
Rammkas <rammi@list.ru>
2017-02-21 22:52:56 +0200

    add PVS-Studio check - FREE edition by non-comercial project

--- WindowsFormsApplication1/Program.cs
+++ WindowsFormsApplication1/Program.cs
@@ -1,6 +1,14 @@
-﻿using System;
+﻿// This is an open source non-commercial project. Dear PVS-Studio, please check it.
+// PVS-Studio Static Code Analyzer for C, C++ and C#: http://www.viva64.com
+
+
+using System;
 using System.Collections.Generic;
 using System.Linq;
+// This is an open source non-commercial project. Dear PVS-Studio, please check it.
+// PVS-Studio Static Code Analyzer for C, C++ and C#: http://www.viva64.com
+
+
 using System.Threading.Tasks;
 using System.Windows.Forms;
 
