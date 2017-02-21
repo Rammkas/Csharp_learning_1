d95466fab060388b9eeb0e1dd56e9214dd1402c7
Rammkas <rammi@list.ru>
2017-02-21 21:58:05 +0200

    Working binary file read/write - OK.

--- WindowsFormsApplication1/Form1.cs
+++ WindowsFormsApplication1/Form1.cs
@@ -8,6 +8,7 @@ using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
 using System.IO;
+//using System.Runtime.InteropServices;
 
 namespace WindowsFormsApplication1
 {
@@ -15,17 +16,17 @@ namespace WindowsFormsApplication1
     {
         public struct Secuence1
         {
-           public int num_of_elements;
            public int[] elements;
         }
 
         public struct FileHeader
         {
-            //public char Identify_of_Data[3];  <-- Hard Way to do -- Marshalling and fixed arrays in struct
+            public string Magic;
             public int Num_of_elements;
-            public int Elements;
         }
-     
+
+        public const int Total_numbers_of_elements = 1000;
+
         public Form1()
         {
             InitializeComponent();
@@ -34,43 +35,63 @@ namespace WindowsFormsApplication1
         private void openToolStripMenuItem_Click(object sender, EventArgs e)
         {
             string fileName;
+            Secuence1 sec1_from_file;
+            FileHeader fh;
+            /*
+             read from file
+              string
+              int
+              int[]
+             */
             if (openFileDialog1.ShowDialog() == DialogResult.OK)
             {
                 fileName = openFileDialog1.FileName;
                 label2.Text = fileName;
-                FileStream file = new FileStream(fileName, FileMode.Open, FileAccess.Read);
-                StreamReader reader = new StreamReader(file);
-                label2.Text = reader.ReadToEnd();
+               
+                BinaryReader reader = new BinaryReader(File.Open(fileName, FileMode.Open)); //создаем бинарный «потоковый читатель» и связываем его с файловым потоком
+                fh.Magic = reader.ReadString();
+                fh.Num_of_elements = reader.ReadInt32();
+                sec1_from_file.elements = new int[fh.Num_of_elements];
+                for (int i = 0; i < fh.Num_of_elements; i++)
+                    sec1_from_file.elements[i] =  reader.ReadInt32();
                 reader.Close();
             }
         }
 
         private void saveToolStripMenuItem_Click(object sender, EventArgs e)
         {
-            string fileName, buff;
+            string fileName;
             Secuence1 sec1_to_file;
-            sec1_to_file.num_of_elements = 0;
+            FileHeader fh;
+            fh.Magic = "Sec";
+           /*
+            write to file
+             string
+             int
+             int[]
+             */
             sec1_to_file.elements =  new int[100];
             if (saveFileDialog1.ShowDialog() == DialogResult.OK)
             {
                 fileName = saveFileDialog1.FileName;
                 label2.Text = fileName;
                 GenerateSecuence(sec1_to_file);
-                buff = Convert.ToString(sec1_to_file);
-                FileStream savefile = new FileStream(fileName, FileMode.Create); //создаем файловый поток
-                StreamWriter writer = new StreamWriter(savefile); //создаем «потоковый писатель» и связываем его с файловым потоком
-                writer.Write(buff); //записываем в файл
+                fh.Num_of_elements = sec1_to_file.elements.Length;
+                BinaryWriter writer = new BinaryWriter(File.Open(fileName, FileMode.Create)); //создаем бинарный «потоковый писатель» и связываем его с файловым потоком
+                writer.Write(fh.Magic); //записываем в файл
+                writer.Write(fh.Num_of_elements);
+                foreach (int el in sec1_to_file.elements)
+                    writer.Write(el);
                 writer.Close(); //закрываем поток. Не закрыв поток, в файл ничего не запишется 
             }      
         }
 
         private void GenerateSecuence(Secuence1 secuence1)
         {
-            for (int i = 0; i < 100; i++)
+            for (int i = 0; i < Total_numbers_of_elements; i++)
             {
                 secuence1.elements[i] = i * 3 + i; 
             }
-            secuence1.num_of_elements = 100;
         }
     }
 }
