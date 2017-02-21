83d63ff923bef1a823a26fa166dc8ab79263f984
Rammkas <rammi@list.ru>
2017-02-21 22:56:26 +0200

    Local savepoint 1

--- WindowsFormsApplication1/Form1.cs
+++ WindowsFormsApplication1/Form1.cs
@@ -12,7 +12,7 @@ using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
 using System.IO;
-//using System.Runtime.InteropServices;
+
 
 namespace WindowsFormsApplication1
 {
--- WindowsFormsApplication1/Program.cs
+++ WindowsFormsApplication1/Program.cs
@@ -5,10 +5,6 @@
 using System;
 using System.Collections.Generic;
 using System.Linq;
-// This is an open source non-commercial project. Dear PVS-Studio, please check it.
-// PVS-Studio Static Code Analyzer for C, C++ and C#: http://www.viva64.com
-
-
 using System.Threading.Tasks;
 using System.Windows.Forms;
 
